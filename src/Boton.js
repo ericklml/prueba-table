9f0b296d4bd6eff318f5552ac2f89d23b2f6c4de
Erick Loera <ericklml69@gmail.com>
2020-12-08 10:54:13 -0600

    first commit

---
+++ src/Boton.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Button  } from 'antd';
+
+export const Boton = ({value, id, month, setState, state}) => {
+
+    const [tipo, setTipo] = useState('default');
+
+    const handleClick = () => {
+        if(tipo === 'default'){
+            setState({...state, [id]:{...state[id],[month]:value}});
+            setTipo('primary');
+            console.log({...state, [id]:{...state[id],[month]:value}});
+        }
+        else{
+            delete state[id][month]
+            setState(state);
+            setTipo('default');
+            console.log(state);
+            Object.keys(state[id]).length === 0 && setState({...state, [id]:true});
+        }
+    }
+
+    return (
+        <Button type={tipo} onClick={handleClick} disabled={!(id in state)}>{value}</Button>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,194 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Table } from 'antd';
+import { Boton } from './Boton';
+import 'antd/dist/antd.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function TreeData() {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const [state, setState] = useState({});
+
+  const columns = [
+    {
+      title: 'id_registro',
+      dataIndex: 'id_registro',
+      key: 'id_registro',
+    },
+    {
+      title: 'CuentaPresupuestalIng',
+      dataIndex: 'CuentaPresupuestalIng',
+      key: 'CueCuentaPresupuestalIngnta',
+    },
+    {
+      title: 'cuenta_nombre',
+      dataIndex: 'cuenta_nombre',
+      key: 'cuenta_nombre',
+    },
+    {
+      title: 'PresupuestoVigente',
+      dataIndex: 'PresupuestoVigente',
+      key: 'PresupuestoVigente',
+    },
+    {
+      title: 'Mar',
+      dataIndex: 'Mar',
+      key: 'Mar',
+      render: (value, records) => <Boton value={value} setState={setState} state={state} month={'Mar'} id={records.key}/>
+    },
+    {
+      title: 'Abr',
+      dataIndex: 'Abr',
+      key: 'Abr',
+      render: (value, records) => <Boton value={value} setState={setState} state={state} month={'Abr'} id={records.key}/>
+    },
+    {
+      title: 'May',
+      dataIndex: 'May',
+      key: 'May',
+      render: (value, records) => <Boton value={value} setState={setState} state={state} month={'May'} id={records.key}/>
+    },
+    {
+      title: 'Jun',
+      dataIndex: 'Jun',
+      key: 'Jun',
+      render: (value, records) => <Boton value={value} setState={setState} state={state} month={'Jun'} id={records.key}/>
+    },
+    {
+      title: 'Total',
+      dataIndex: 'Total',
+      key: 'Total',
+    },
+    {
+      title: 'Diferencia',
+      dataIndex: 'Diferencia',
+      key: 'Diferencia',
+    },
+  ];
+
+  const data = [
+    {
+      key: 1,
+      "id_registro": 7,
+      "CuentaPresupuestalIng": "17",
+      "cuenta_nombre": "Accesorios de Impuestos",
+      "PresupuestoVigente": 130000,
+      "Mar": 42919,
+      "Abr": 0,
+      "May": 0,
+      "Jun": 0,
+      "Total": 42919,
+      "Diferencia": 87081,
+      "children": [
+        {
+          key: 2,
+          "id_registro": 8,
+          "CuentaPresupuestalIng": "17",
+          "cuenta_nombre": "Accesorios de Impuestos",
+          "PresupuestoVigente": 130000,
+          "Mar": 42919,
+          "Abr": 0,
+          "May": 0,
+          "Jun": 0,
+          "Total": 42919,
+          "Diferencia": 87081,
+          "children": [
+            {
+              key: 3,
+              "id_registro": 9,
+              "CuentaPresupuestalIng": "17-01",
+              "cuenta_nombre": "Recargos",
+              "PresupuestoVigente": 130000,
+              "Mar": 42919,
+              "Abr": 0,
+              "May": 0,
+              "Jun": 0,
+              "Total": 42919,
+              "Diferencia": 87081
+            }
+          ]
+        },
+      ]
+    },
+    {
+      key: 4,
+      "id_registro": 10,
+      "CuentaPresupuestalIng": "17",
+      "cuenta_nombre": "Accesorios de Impuestos",
+      "PresupuestoVigente": 130000,
+      "Mar": 42919,
+      "Abr": 0,
+      "May": 0,
+      "Jun": 0,
+      "Total": 42919,
+      "Diferencia": 87081,
+      "children": [
+        {
+          key: 5,
+          "id_registro": 11,
+          "CuentaPresupuestalIng": "17",
+          "cuenta_nombre": "Accesorios de Impuestos",
+          "PresupuestoVigente": 130000,
+          "Mar": 42919,
+          "Abr": 0,
+          "May": 0,
+          "Jun": 0,
+          "Total": 42919,
+          "Diferencia": 87081,
+          "children": [
+            {
+              key: 6,
+              "id_registro": 12,
+              "CuentaPresupuestalIng": "17-01",
+              "cuenta_nombre": "Recargos",
+              "PresupuestoVigente": 130000,
+              "Mar": 42919,
+              "Abr": 0,
+              "May": 0,
+              "Jun": 0,
+              "Total": 42919,
+              "Diferencia": 87081
+            },
+            {
+              key: 7,
+              "id_registro": 13,
+              "CuentaPresupuestalIng": "17-02",
+              "cuenta_nombre": "Recargos",
+              "PresupuestoVigente": 130000,
+              "Mar": 42919,
+              "Abr": 0,
+              "May": 0,
+              "Jun": 0,
+              "Total": 42919,
+              "Diferencia": 87081
+            }
+          ]
+        },
+      ]
+    },
+  ];
+
+  // rowSelection objects indicates the need for row selection
+  const rowSelection = {
+    onSelect: (record, selected, selectedRows) => {
+      console.log(record, selected, selectedRows);
+      let temp = {};
+      selectedRows.map(row => {
+        temp[row.key] = true;
+      });
+      setState(temp);
+      console.log(temp);
+    }
+  };
+
+  return (
+    <>
+      <Table
+        columns={columns}
+        rowSelection={{ ...rowSelection, checkStrictly: false }}
+        dataSource={data}
+      />
+    </>
+  );
+}
+
+ReactDOM.render(<TreeData />, document.getElementById('root'));
